e3303a6eb2417ac34be8187c5f005d9d396466b5
Mariia Verbylo <mariaverbilo@gmail.com>
2022-11-14 18:29:46 +0200

    Refac BtnDiv

--- src/components/BtnDiv/index.jsx
+++ src/components/BtnDiv/index.jsx
@@ -2,24 +2,39 @@ import React from 'react';
 
 import styles from './Btn.module.css';
 const BtnDiv = ({ isStarted, handleStart, handleReset, addCirle }) => {
-  const { redBtn, greenBtn, whiteBtn } = styles;
-  return (
-    <div className={styles.btnWrapper}>
-      <button className={isStarted ? redBtn : greenBtn} onClick={handleStart}>
-        {isStarted ? 'Stop' : 'Start'}
-      </button>
-      <button className={whiteBtn} onClick={handleReset}>
-        Reset
-      </button>
-      <button onClick={addCirle}>
-        <img
-          className={whiteBtn}
-          alt="circle"
-          src="https://cdn0.iconfinder.com/data/icons/arrows-03/100/arrows-55-512.png"
-        />
-      </button>
-    </div>
+  const { btnWrapper, redBtn, greenBtn, whiteBtn } = styles;
+  const img = (
+    <img
+      alt="circle"
+      src="https://cdn0.iconfinder.com/data/icons/arrows-03/100/arrows-55-512.png"
+    />
   );
+  const dataBtn = [
+    {
+      id: 1,
+      className: isStarted ? redBtn : greenBtn,
+      onClick: handleStart,
+      text: isStarted ? 'Stop' : 'Start',
+    },
+    {
+      id: 2,
+      className: whiteBtn,
+      onClick: handleReset,
+      text: 'Reset',
+    },
+    {
+      id: 2,
+      className: whiteBtn,
+      onClick: addCirle,
+      text: img,
+    },
+  ];
+  const btns = dataBtn.map(({ id, className, onClick, text }) => (
+    <button key={id} className={className} onClick={onClick}>
+      {text}
+    </button>
+  ));
+  return <div className={btnWrapper}>{btns}</div>;
 };
 
 export default BtnDiv;
